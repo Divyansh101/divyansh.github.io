72b7d2d83f09d01764fa94c9fa1a4710d841b26d
unknown <khatri.divyansh98@gmail.com>
2020-09-23 21:08:47 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Typewriter } from 'react-typewriting-effect'
 import 'react-typewriting-effect/dist/index.css'
-
+import dp from './imgs/about.jpeg';
 class App extends Component  {
 
 
@@ -20,18 +20,26 @@ class App extends Component  {
 		}
 
 		return (
+
 			<div className = "main">
+
 				<div>
 					<ul>
-					<li><a className = "active" href="#home">Home</a></li>
-					<li><a href="#news">News</a></li>
-					<li><a href="#contact">Contact</a></li>
-					<li><a href="#about">About</a></li>
+					<li>
+						<div className = "img">
+							<img src = {dp} alt = "dp"/>
+						</div>
+					</li>
+					<li>
+						<div className = "img">
+						<img alt = "home" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAABPUlEQVRIieWSvUoDQRSFzwQhFsEUdhJSaSE+gAgWkVRBwcrO2kqxtLVJmwfQ1kKsIj6CwSadNlpoZxGjoFaLWfgsMsU4rOtssoKSA1PMnfNz78xIEwOgAnSAS6CSt3kN6AFvdvWAWl7mO8AHcAcsAQvADRADB4AZ1bgEnDLEOVD2zs7sWRuYyWo+D1zbLg+TugQMsG+nuwUWQ803gFfgCagH8OtA32rWQ8gDoAtUgzoa6qpWMwDW0ohXwL173xlCysAD0EkjRUArq7mjbwGRWyt4nKKk91EDrLaYFpA7pkJIwKykbUnTthRJOjHGvOQSIOlC0opX25K0+pMw9IqWJTWNhaRmQuBYAQVJsbOPQ7W//sj/P+ALgGe+x67D20vh9V1P/5s2JG0m1B8lHTv7I0lG0pzHiyW1xxny7+ETq2E2WTTWCnsAAAAASUVORK5CYII="></img>
+						</div>
+					</li>
 					</ul>
 				</div>
 				<div className = "back">
-					<p className = "backTextUp">Web</p>
-					<p className = "backTextDown">Developer</p>
+					<p className = "backTextUp">DEVE</p>
+					<p className = "backTextDown">LOP</p>
 				</div>
 				<div className = "App">
 					<div className = "paradiv">
@@ -43,15 +51,6 @@ class App extends Component  {
 					/>
 					{button}
 					</div>
-
-					{/* <div>
-					<p className="paragraph" >I am</p>
-					
-					</div>
-					<div>
-					<p className="paragraph" >Divyansh</p>
-
-					</div>  */}
 					
 				</div>
 
