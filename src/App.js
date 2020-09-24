ee1f968c58c479e8bfbf252635be0319250f0813
unknown <khatri.divyansh98@gmail.com>
2020-09-24 17:20:14 +0530

    redesign UI

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import cr from './imgs/circle-red.png';
 import cb from './imgs/shape-1.png';
-import cp from './imgs/circle-pink.png';
 import tr from './imgs/triangle-red.png';
+import sp from './imgs/shape-1.png';
 import tb from './imgs/triangle-blue.png';
 
 class App extends Component  {
@@ -24,7 +24,9 @@ class App extends Component  {
 				</div>
 				<div className = "greet">
 					<p className = "paragraph">Hello <span className = "image-cr" ><img src = {cr}/></span><br />I am<br /><div className = "name">Divyansh </div></p>
-					<span className = "image-cp"><img src = {cp} /></span>
+					<span className = "image-cp"><img src = {sp} /></span>
+					<span className = "image-tr"><img src = {tr} /></span>
+					<span className = "image-tb"><img src = {tb} /></span>
 				</div>
 				<div className = "pointsOuter">
 					<p className = "points">
@@ -40,6 +42,7 @@ class App extends Component  {
 				</div>
 				<div>
 					<img className = "image-cb" src = {cb}/>
+					<img className = "image-clb" src = {cb}/>
 				</div>
 				<div>
 				<p className ="name-outline">Divyansh</p>
