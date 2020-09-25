76581ff3401737fb3ac4d1f3991faa0e5144cedc
unknown <khatri.divyansh98@gmail.com>
2020-09-25 16:44:43 +0530

    Updates

---
+++ precache-manifest.b05b2d26e676dd13664d5c079cd6d218.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e4bff2de8e0cfc15d8f188c261c90f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "82f7fdb4ec66dae467e4",
+    "url": "/portfolio/static/css/main.405d0afc.chunk.css"
+  },
+  {
+    "revision": "0eee34eaf52cd49e5c6e",
+    "url": "/portfolio/static/js/2.810cedcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.810cedcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82f7fdb4ec66dae467e4",
+    "url": "/portfolio/static/js/main.79f17b71.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "73b9994bb505da43a387d107849baf95",
+    "url": "/portfolio/static/media/me-r.73b9994b.otf"
+  },
+  {
+    "revision": "3b24078346a6ae8b9caeca47834fcea9",
+    "url": "/portfolio/static/media/me-ub.3b240783.otf"
+  },
+  {
+    "revision": "dba0c688b8d5ee09a1e214aebd5d25e4",
+    "url": "/portfolio/static/media/ps-b.dba0c688.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/divyansh101.github.io/precache-manifest.380bf75bc6188ea025feac65fd31cddb.js"
+  "/portfolio/precache-manifest.b05b2d26e676dd13664d5c079cd6d218.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/divyansh101.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
